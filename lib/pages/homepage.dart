871365bd3d6ba3b533719c1f93360c5e7503843f
ss0313 <76594980+ss0313@users.noreply.github.com>
2021-06-08 00:03:58 +0530

    Add files via upload

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -14,7 +14,46 @@ class _HomePageState extends State<HomePage> {
       home:DefaultTabController(
       length: 2,
         child: Scaffold(
-          appBar: appBar(),
+          appBar: appBar(
+
+          ),
+
+          drawer: Drawer(
+            // Add a ListView to the drawer. This ensures the user can scroll
+            // through the options in the drawer if there isn't enough vertical
+            // space to fit everything.
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                  child: Text('Drawer Header'),
+                ),
+                ListTile(
+                  title: Text('Item 1'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text('Item 2'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ),
+
          bottomNavigationBar: new Material(
                 color: Colors.pink,
          ),
