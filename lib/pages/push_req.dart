c6e7b9c33dd322f0bd573b4c06b9ca4f86477279
ss0313 <76594980+ss0313@users.noreply.github.com>
2021-06-08 19:18:37 +0530

    Add files via upload

--- lib/pages/push_req.dart
+++ lib/pages/push_req.dart
@@ -33,7 +33,8 @@ class _PushRequestState extends State<PushRequest> {
                     type: item['type'],
                     duration: item['day'],
                     time: item['time'],
-                    email:item['email']
+                    email:item['email'],
+                    status: item['status']
                   ),
                   actions: [
                     IconSlideAction(
@@ -77,34 +78,40 @@ List<Map<String, String>> testData = [
     "type": "Theory",
     "time": "8 am",
     "day": "Tuesday",
-    "email" :"gandhi18@iitg.ac.in"
+    "email" :"gandhi18@iitg.ac.in",
+    "status":"cancelled"
+
   },
   {
     "title": "CS 223",
     "type": "Theory",
     "time": "9 am",
     "day": "Thursday",
-    "email" :"gandhi18@iitg.ac.in"
+    "email" :"gandhi18@iitg.ac.in",
+    "status":"cancelled"
   },
   {
     "title": "CS 110",
     "type": "Assignment",
     "time": "12 am",
     "day": "Monday",
-    "email" :"gandhi18@iitg.ac.in"
+    "email" :"gandhi18@iitg.ac.in",
+    "status":"update"
   },
   {
     "title": "CS 245",
     "type": "Quiz",
     "time": "11 am",
     "day": "Friday",
-    "email" :"gandhi18@iitg.ac.in"
+    "email" :"gandhi18@iitg.ac.in",
+    "status":"update"
   },
   {
     "title": "HSS 123",
     "type": "Quiz",
     "time": "2 pm",
     "day": "3 hour",
-    "email" :"gandhi18@iitg.ac.in"
+    "email" :"gandhi18@iitg.ac.in",
+    "status":"add"
   },
 ];
--- lib/widgets/myListTile.dart
+++ lib/widgets/myListTile.dart
@@ -3,6 +3,12 @@ import 'package:timetable_2_flutter_admin/globals/myColors.dart';
 import 'package:timetable_2_flutter_admin/globals/myFonts.dart';
 import 'package:timetable_2_flutter_admin/globals/mySpaces.dart';
 
+import '../globals/myColors.dart';
+import '../globals/myColors.dart';
+import '../globals/myColors.dart';
+import '../globals/myColors.dart';
+import '../globals/myColors.dart';
+
 // ignore: must_be_immutable
 class MyListTile extends StatelessWidget {
   final String title;
@@ -10,18 +16,36 @@ class MyListTile extends StatelessWidget {
   final String duration;
   final String type;
   final String email;
+  final String status;
   Color sideColor;
-
-  MyListTile({this.title, this.type, this.time, this.duration, this.email}) {
-    switch (this.type) {
-      case 'Assignment':
+  Color mainColor;
+  Color text;
+  Color bgColor;
+  MyListTile(
+      {this.title,
+      this.status,
+      this.type,
+      this.time,
+      this.duration,
+      this.email}) {
+    switch (this.status) {
+      case 'cancelled':
         sideColor = kBlack;
+        mainColor = k1Red;
+        text = kRed;
+        bgColor=lRed;
         break;
-      case 'Quiz':
-        sideColor = kYellow;
+      case 'update':
+        sideColor = kBlue;
+        mainColor = k1Yellow;
+        text = kYellow;
+        bgColor=lYellow;
         break;
       default:
         sideColor = kBlue;
+        mainColor = kBlue;
+        bgColor=lBlue;
+        text = kBlue;
     }
   }
 
@@ -33,7 +57,8 @@ class MyListTile extends StatelessWidget {
       },
       child: Container(
         child: Padding(
-          padding: EdgeInsets.fromLTRB(MySpaces.listTileLeftPadding, 0, 0, MySpaces.horizontalScreenPadding),
+          padding: EdgeInsets.fromLTRB(MySpaces.listTileLeftPadding, 0, 0,
+              MySpaces.horizontalScreenPadding),
           child: Row(
             children: [
               Expanded(
@@ -49,6 +74,7 @@ class MyListTile extends StatelessWidget {
                         ),
                       ),
                     ),
+                    
                     Expanded(
                       child: Row(
                         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -68,16 +94,39 @@ class MyListTile extends StatelessWidget {
                   ],
                 ),
               ),
+              Container(
+                decoration: BoxDecoration(
+                    color: bgColor,
+                  borderRadius: BorderRadius.circular(18.25)
+                ),
+                height: 20,
+                width: 79,
+
+                child: SizedBox(
+
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      '$status',
+                      style:
+                        MyFonts.medium.setColor(text).size(10),
+                    ),
+                  ),
+                ),
+              ),
               Expanded(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Expanded(
-                      child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Text(
-                          '$time',
-                          style: MyFonts.extraBold.size(20),
+                    Container(
+                      height: 20,
+                      child: Expanded(
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Text(
+                            '$time',
+                            style: MyFonts.extraBold.size(20),
+                          ),
                         ),
                       ),
                     ),
@@ -90,7 +139,7 @@ class MyListTile extends StatelessWidget {
                         ),
                       ),
                     ),
-               Expanded(
+                    Expanded(
                       child: Align(
                         alignment: Alignment.bottomCenter,
                         child: Text(
@@ -110,8 +159,8 @@ class MyListTile extends StatelessWidget {
           gradient:
               LinearGradient(stops: [0.05, 0.05], colors: [sideColor, kWhite]),
           color: kWhite,
-          // border: Border.all(color: kGrey, width: 1),
-          // borderRadius: BorderRadius.circular(15),
+           border: Border.all(color: grey2, width: 1),
+           borderRadius: BorderRadius.circular(4),
         ),
       ),
     );
